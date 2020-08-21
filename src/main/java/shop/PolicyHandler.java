9cb4d9f90ff408ac39c5563ecf834a01132b4a7b
bzness <bzness@naver.com>
2020-08-21 13:14:15 +0900

    2nd commit

--- src/main/java/shop/PolicyHandler.java
+++ src/main/java/shop/PolicyHandler.java
@@ -1,12 +1,12 @@
 package shop;
 
-import shop.config.kafka.KafkaProcessor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+import shop.config.kafka.KafkaProcessor;
+
+import java.util.Optional;
 
 @Service
 public class PolicyHandler{
@@ -15,5 +15,22 @@ public class PolicyHandler{
 
     }
 
+    @Autowired
+    OrderRepository orderRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverShipped_UpdateStatus(@Payload Shipped shipped){
+
+        if(shipped.isMe()){
+
+            // 배송상태에 따른 주문서비스의 배송 상태값 업데이트. (Delivery 서비스의 Shipped 이벤트에 Listening 하는 코드 작성)
+            Optional<Order> orderOptional = orderRepository.findById(shipped.getOrderId());
+            Order order = orderOptional.get();
+            order.setStatus(shipped.getStatus());
+
+            orderRepository.save(order);
+
+        }
+    }
 
 }
